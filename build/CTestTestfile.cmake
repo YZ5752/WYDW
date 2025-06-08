eb420ea2d9d33b5f663df68aee6f3cc0876711f1
zyh <2365174401@qq.com>
2025-06-08 03:58:29 -0700

    配置数据库

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhang/Desktop/WYDW1
-# Build directory: /home/zhang/Desktop/WYDW1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
